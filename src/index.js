fb6ce1eeac2736709327c9e60df4625c4402d7ec
Kate <likhvar.ks@gmail.com>
2021-03-03 16:04:12 +0200

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    
+ if (n < 0) {
+    n *= -1;
+  }
+
+str = '' + n;
+str2 = '';
+
+for (let i = str.length-1; i >= 0; i--){
+  str2 += str[i];
+}
+return +str2;
 }
